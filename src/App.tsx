68841c7eb282128fe35e8a2e09f7cab982ede793
chrid97 <cgoncal12@gmail.com>
2021-12-27 13:36:08 -0500

    Add slider componenet

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import { Slider } from "./components/Slider";
 
 function App() {
-  return <p></p>
+  return (
+    <div className="bg-dark-900 max-w-screen min-h-screen">
+      <aside className="w-[260px] h-screen bg-dark-800 text-[#FFFFFF] px-5">
+        <Slider text={"Red"} min={-255} max={255} initialValue={0}></Slider>
+        <Slider text={"Green"} min={-255} max={255} initialValue={0}></Slider>
+        <Slider text={"Blue"} min={-255} max={255} initialValue={0}></Slider>
+      </aside>
+    </div>
+  );
 }
 
 export default App;
